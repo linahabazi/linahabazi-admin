55096fedcef52ca1877a3de734bdcd436d98f2c6
Lina <lhabazil@gmail.com>
2022-11-29 23:44:42 -0600

    under construction message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Lina Habazi Admin Portal
+          Under Construction
         </p>
       </header>
     </div>
