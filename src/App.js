fa2ad2d873159f0ce3c716329b6f0d6fb01afb1d
Lina <lhabazil@gmail.com>
2022-12-01 23:26:27 -0600

    dummy dataProvider and some folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin";
 import Tutorial from './react-admin/tutorial';
 
+import dataProvider from './providers/dataProvider'
+
 const App = () => (
-  <Tutorial />
+  // <Tutorial />
+  <Admin dataProvider={dataProvider} >
+    {/* <Resource name="users" list={UserList} recordRepresentation={"name"} icon={UserIcon} />
+    <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} /> */}
+  </Admin>
 );
 
 export default App;
